01b402a0eb62eed78accfd4c772e1ce0e4fd8a27
Pete Sevander <pete@sevander.com>
2016-03-15 09:10:11 +0200

    tabs to spaces and indentation

--- src/app_message.c
+++ src/app_message.c
@@ -1,44 +1,44 @@
 #include <pebble.h>
 
-static Window *s_window;	
-	
+static Window *s_window;
+
 // Keys for AppMessage Dictionary
 // These should correspond to the values you defined in appinfo.json/Settings
 enum {
-	STATUS_KEY = 0,	
-	MESSAGE_KEY = 1
+    STATUS_KEY = 0,
+    MESSAGE_KEY = 1
 };
 
 // Write message to buffer & send
 static void send_message(void){
-	DictionaryIterator *iter;
-	
-	app_message_outbox_begin(&iter);
-	dict_write_cstring(iter, MESSAGE_KEY, "I'm a Pebble!");
-	
-	dict_write_end(iter);
-  app_message_outbox_send();
+    DictionaryIterator *iter;
+
+    app_message_outbox_begin(&iter);
+    dict_write_cstring(iter, MESSAGE_KEY, "I'm a Pebble!");
+
+    dict_write_end(iter);
+    app_message_outbox_send();
 }
 
 // Called when a message is received from PebbleKitJS
 static void in_received_handler(DictionaryIterator *received, void *context) {
-	Tuple *tuple;
-	
-	tuple = dict_find(received, STATUS_KEY);
-	if(tuple) {
-		APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Status: %d", (int)tuple->value->uint32); 
-	}
-	
-	tuple = dict_find(received, MESSAGE_KEY);
-	if(tuple) {
-		APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Message: %s", tuple->value->cstring);
-	}
-  
-  send_message();
+    Tuple *tuple;
+
+    tuple = dict_find(received, STATUS_KEY);
+    if(tuple) {
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Status: %d", (int)tuple->value->uint32);
+    }
+
+    tuple = dict_find(received, MESSAGE_KEY);
+    if(tuple) {
+        APP_LOG(APP_LOG_LEVEL_DEBUG, "Received Message: %s", tuple->value->cstring);
+    }
+
+    send_message();
 }
 
 // Called when an incoming message from PebbleKitJS is dropped
-static void in_dropped_handler(AppMessageResult reason, void *context) {	
+static void in_dropped_handler(AppMessageResult reason, void *context) {
 }
 
 // Called when PebbleKitJS does not acknowledge receipt of a message
@@ -46,27 +46,27 @@ static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reas
 }
 
 static void init(void) {
-	s_window = window_create();
-	window_stack_push(s_window, true);
-	
-	// Register AppMessage handlers
-	app_message_register_inbox_received(in_received_handler); 
-	app_message_register_inbox_dropped(in_dropped_handler); 
-	app_message_register_outbox_failed(out_failed_handler);
+    s_window = window_create();
+    window_stack_push(s_window, true);
 
-  // Initialize AppMessage inbox and outbox buffers with a suitable size
-  const int inbox_size = 128;
-  const int outbox_size = 128;
-	app_message_open(inbox_size, outbox_size);
+    // Register AppMessage handlers
+    app_message_register_inbox_received(in_received_handler);
+    app_message_register_inbox_dropped(in_dropped_handler);
+    app_message_register_outbox_failed(out_failed_handler);
+
+    // Initialize AppMessage inbox and outbox buffers with a suitable size
+    const int inbox_size = 128;
+    const int outbox_size = 128;
+    app_message_open(inbox_size, outbox_size);
 }
 
 static void deinit(void) {
-	app_message_deregister_callbacks();
-	window_destroy(s_window);
+    app_message_deregister_callbacks();
+    window_destroy(s_window);
 }
 
 int main( void ) {
-	init();
-	app_event_loop();
-	deinit();
+    init();
+    app_event_loop();
+    deinit();
 }
